e430dcec288d740c8e430802b91c0168c44e18a0
O1sims <sims.owen@gmail.com>
2019-04-20 22:12:57 +0100

    Update tweet analysis for BBC NI news

--- R/tweet_analysis.R
+++ R/tweet_analysis.R
@@ -5,19 +5,16 @@ library(lubridate)
 
 
 
+bbc.ni.tweets.json <- getwd() %>% 
+  paste0("/data/bbc/bbcnewsni.json") %>% 
+  jsonlite::read_json()
+
 jp.tweets.json <- getwd() %>% 
   paste0("/data/bbc/jp_biz.json") %>% 
   jsonlite::read_json()
 
-rm.tweets.json <- getwd() %>% 
-  paste0("/data/belfast-telegraph/ryanmcaleerbiz.json") %>% 
-  jsonlite::read_json()
-
-jm.tweets.json <- getwd() %>% 
-  paste0("/data/ulster-business/newsmulg.json") %>% 
-  jsonlite::read_json()
-
 news.tweets.json <- c(
+  bbc.ni.tweets.json,
   jp.tweets.json)
 
 tweetId <- datetime <- favourites <- 
@@ -39,7 +36,7 @@ for (i in 1:length(news.tweets.json)) {
     "leave", "cameron", "merkel", "macron", "remain", "wto", "border", "backstop", "tusk", "theresa may", "barnier", "exit"), collapse = "|"), tweetText, ignore.case = TRUE)) {
     theme %<>% append(" UK/EU Relations")
   } else if (grepl(paste(c("education", "school", "skill", "universit", "qub", "uu"), collapse = "|"), tweetText, ignore.case = TRUE)) {
-    theme %<>% append("Education")
+    theme %<>% append("Education & Skills")
   } else if (grepl(paste(c("york street", "street", "transport", "interchange", "a5", "road"), collapse = "|"), tweetText, ignore.case = TRUE)) {
     theme %<>% append("Infrastructure")
   } else if (grepl(paste(c("stormont", "assembly", "executive", "minister"), collapse = "|"), tweetText, ignore.case = TRUE)) {
@@ -48,18 +45,22 @@ for (i in 1:length(news.tweets.json)) {
     theme %<>% append("Energy")
   } else if (grepl(paste(c("productivity", "innovation"), collapse = "|"), tweetText, ignore.case = TRUE)) {
     theme %<>% append("Productivity")
+  } else if (grepl(paste(c("equality", "lgbt", "abortion", "rights", "gay", "lesbian"), collapse = "|"), tweetText, ignore.case = TRUE)) {
+    theme %<>% append("Rights & Equality")
+  } else if (grepl(paste(c("nhs", "hospital", "healthcare"), collapse = "|"), tweetText, ignore.case = TRUE)) {
+    theme %<>% append("Healthcare")
   } else {
     theme %<>% append("unknown")
   }
-  if (grepl(paste(c("dup", "democratic unionist party"), collapse = "|"), tweetText, ignore.case = TRUE)) {
+  if (grepl(paste(c("dup", "democratic unionist party", "arlene", "foster", "sammy wilson", "nigel dodds"), collapse = "|"), tweetText, ignore.case = TRUE)) {
     party %<>% append("DUP")
-  } else if (grepl("sinn fein", tweetText, ignore.case = TRUE)) {
-    party %<>% append("Sinn Fein")
-  } else if (grepl(paste(c("uup", "ulster unionist party"), collapse = "|"), tweetText, ignore.case = TRUE)) {
+  } else if (grepl(c("sinn fein", "sinn féin", "mary lou mcdonald", "michelle o'neill"), tweetText, ignore.case = TRUE)) {
+    party %<>% append("Sinn Féin")
+  } else if (grepl(paste(c("uup", "ulster unionist party", "robin swann", "may steele", "reg empey"), collapse = "|"), tweetText, ignore.case = TRUE)) {
     party %<>% append("UUP")
-  } else if (grepl(paste(c("sdlp", "social democratic and labour party"), collapse = "|"), tweetText, ignore.case = TRUE)) {
+  } else if (grepl(paste(c("sdlp", "social democratic and labour party", "colum eastwood", "nichola mallon"), collapse = "|"), tweetText, ignore.case = TRUE)) {
     party %<>% append("SDLP")
-  } else if (grepl(paste(c("alliance"), collapse = "|"), tweetText, ignore.case = TRUE)) {
+  } else if (grepl(paste(c("alliance", "naomi long", "stephen farry"), collapse = "|"), tweetText, ignore.case = TRUE)) {
     party %<>% append("Alliance")
   } else {
     party %<>% append("unknown")
@@ -71,18 +72,19 @@ news.tweets.df <- data.frame(
   tweetId, ymd(datetime), favourites, 
   retweets, text, theme, party,
   stringsAsFactors = FALSE) %>% 
-  subset(theme != "unknown")
+  subset(party != "unknown")
 
 news.tweets.df %>%
   ggplot() +
   geom_histogram(
     mapping = aes(
       x = news.tweets.df$ymd.datetime., 
-      fill = news.tweets.df$theme),
+      fill = news.tweets.df$party),
     colour = "#EFEFEF",
     bins = 68) + 
-  xlab("") + ylab("Count") +
-  ggthemes::scale_fill_tableau() +
+  xlab("") + ylab("") +
+  ggthemes::scale_fill_tableau(name="") +
+  scale_x_date(date_breaks = "years" , date_labels = "%Y") +
   theme_minimal() +
   theme(legend.position = "bottom")
 
